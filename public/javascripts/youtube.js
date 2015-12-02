fd8d2dde72c165910d7024cf621d4e1fe55f06fe
toledoJavier <toledojavier90@gmail.com>
2015-12-02 13:27:53 -0300

    Refactor js de youtube api

---
+++ public/javascripts/youtube.js
@@ -0,0 +1,14 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: $("#player").attr("videoID"),
+  });
+}
